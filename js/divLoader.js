06b16e19f95784dd81e91b2aef2f9d6127c160dd
Ashivni Shekhawat <shekhawat.ashivni@gmail.com>
2014-04-23 14:33:41 -0700

    A fairly final version. Many changes.

--- js/divLoader.js
+++ js/divLoader.js
@@ -1,38 +1,75 @@
 $(document).ready(function() {
+	var p=window.location.hash;
+	if (p=='#research')
+	{
+		$('#content').load('pages/research.html #c1');
+	}
+	if (p=='#pub')
+	{
+		$('#content').load('pages/publications.html #c1');
+	}
+	if (p=='#cv')
+	{
+		$('#content').load('pages/cv.html #c1');
+	}
 						   
-	var hash = window.location.hash.substr(1);
-	//console.log(window.location.hash);
-	//console.log('Hash: ' + hash);
-	var href = $('#nav li a').each(function(){
-		var href = $(this).attr('href');
-		//console.log('Loop1: ' + href)
-		if(hash==href.substr(0,href.length-5)){
-			var toLoad = hash+'.html #c1';
-			$('#content').load(toLoad)
-		}											
-	});
-
-	var href = $('#nav li ul li a').each(function(){
-		var href = $(this).attr('href');
-		//console.log('Loop 2: ' +href)
-		if(hash==href.substr(0,href.length-5)){
-			var toLoad = hash+'.html #c1';
-			$('#content').load(toLoad)
-		}											
-	});
-
 	$('#nav li a').click(function(){
 		var toLoad = $(this).attr('href')+' #c1';
-		//$('#content').load(toLoad);
 		$("#content").load(toLoad, function() {
-			$('#content').scrollTop(1);
+			$('body').scrollTop(1);
 			});
-		/*
-		$("#content").load(toLoad, function(rt,ts,xm) {
-		  console.log( "Load was performed. " + toLoad  );
-			console.log('Response text: ' + rt);
-			});*/
 		return false;
 	});
 
+
+
 });
+
+
+var delay=3000 //set delay in miliseconds
+var curindex=0
+
+var randomimages=new Array()
+var captions = new Array()
+
+randomimages[0]="images/fccSM.jpg"
+captions[0] = "High Entropy Alloy"
+
+randomimages[1]="images/nacreSM.jpg"
+captions[1] = "Nacre: Nature's flaw tolerant architecture"
+
+randomimages[2]="images/fracturePhaseSM.jpg"
+captions[2] = "Phase diagram of brittle fracture"
+
+randomimages[3]="images/fuseNetworkSM.jpg"
+captions[3] = "A fuse network"
+
+randomimages[4]="images/stressConcSM.jpg"
+captions[4] = "Stress field in a fuse network simulation"
+
+var preload=new Array()
+
+for (n=0;n<randomimages.length;n++)
+{
+	preload[n]=new Image()
+	preload[n].src=randomimages[n]
+}
+
+
+
+function rotateimage()
+{
+	/*
+	if (curindex==(tempindex=Math.floor(Math.random()*(randomimages.length)))){
+		curindex=curindex==0? 1 : curindex-1
+	}
+	else
+			curindex=tempindex
+	*/
+	curindex = (curindex+1)%randomimages.length
+	$('#pic').attr('src',randomimages[curindex]);
+	$('#picCap').text(captions[curindex]);
+}
+
+setInterval("rotateimage()",delay)
+
