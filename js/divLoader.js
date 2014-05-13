2c2e1f10d4167108b9e723ce1281916d1a9b74ef
Ashivni Shekhawat <shekhawat.ashivni@gmail.com>
2014-05-12 23:13:43 -0700

    Section on nacre elastic constants

--- js/divLoader.js
+++ js/divLoader.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
 		var toLoad = $(this).attr('href')+' #c1';
 		$("#content").load(toLoad, function() {
 			$('body').scrollTop(1);
+			MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 			});
 		return false;
 	});
@@ -32,19 +33,19 @@ var curindex=0
 var randomimages=new Array()
 var captions = new Array()
 
-randomimages[0]="images/fccSM.jpg"
+randomimages[0]="images/fcc.jpg"
 captions[0] = "High Entropy Alloys"
 
-randomimages[1]="images/nacreSM.jpg"
+randomimages[1]="images/nacre.jpg"
 captions[1] = "Biomaterials"
 
-randomimages[2]="images/fracturePhaseSM.jpg"
+randomimages[2]="images/fracturePhase.jpg"
 captions[2] = "Phase Transitions"
 
-randomimages[3]="images/fuseNetworkSM.jpg"
+randomimages[3]="images/fuseNetwork.jpg"
 captions[3] = "Statistical Models"
 
-randomimages[4]="images/stressConcSM.jpg"
+randomimages[4]="images/stressConc.jpg"
 captions[4] = "Massive Simulations"
 
 var preload=new Array()
